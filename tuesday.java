ef642086847138bb891e8929938628719bba0ca4
deepuslk <37328405+deepuslk@users.noreply.github.com>
2018-03-13 17:00:10 +0530

    Update tuesday.java

--- tuesday.java
+++ tuesday.java
@@ -1,11 +1,10 @@
-int i;
 class Tuesday
 {
 public static void main(String args[])
 {
-for (i=0;i<=10;i++)
+for (int i=0;i<=10;i++)
 {
-System.out.println("today is tuesday",i);
+System.out.println("today is tuesday",i+);
 }
 }
 }
